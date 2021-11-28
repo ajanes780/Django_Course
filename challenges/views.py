01c501b3565ed60abc21de100e1c930a72428ed3
Aaron Janes <aaron.janes@gmail.com>
2021-11-28 00:45:22 -0700

    routes
    WIP
    wip

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def monthlychallenge(request, month):
+    print(month)
+    if month == "march":
+      return HttpResponse("its your birthday !")
+
+    return HttpResponse("Some really cool stuff")
