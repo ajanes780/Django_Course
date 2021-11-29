e872df6b1f6c63526be408bbe1232f70fde5d3f7
Aaron Janes <aaron.janes@gmail.com>
2021-11-28 22:51:29 -0700

    templating

--- challenges/views.py
+++ challenges/views.py
@@ -4,13 +4,12 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.urls import reverse
 
-
 monthly_challenges = {
-    "jan": " run 5 miles every day ",
-    "feb": "Study python 30 mins a day ",
-    "march": "its your birthday",
+    "jan": "Run 5 miles every day",
+    "feb": "Study python 30 mins a day",
+    "march": "It's your birthday",
     "april": "Easter is coming",
-    "may": "get ready for spring",
+    "may": "Get ready for spring",
     "june": "keep working out",
     "july": "how far can you run ",
     "aug": "Ready for the beach",
@@ -24,23 +23,18 @@ months = list(monthly_challenges.keys())
 
 
 def index(req):
+    list_items = ""
+    for month in months:
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{month.capitalize()}</a></li>"
+        response_body = f"<ul>{list_items}</ul>"
 
-  list_items = ""
-
-  for month in months:
-   
-    month_path = reverse("month-challenge", args=[month])
-    list_items += f"<li><a href=\"{month_path}\">{month.capitalize()}</a></li>"
-    response_body = f"<ul>{list_items}</ul>"
-
-  return HttpResponse(response_body)
+    return HttpResponse(response_body)
 
 
 def monthly_challenge_by_number(request, month):
-
     if month != 0 and month <= len(months):
         redirect_month = months[month-1]
-
         redirect_path = reverse("month-challenge", args=[redirect_month])
         return HttpResponseRedirect(redirect_path)
 
@@ -50,7 +44,9 @@ def monthly_challenge_by_number(request, month):
 def monthlychallenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_markup = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_markup)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month": month
+        })
     except:
         return HttpResponseNotFound("This month is not supported")
